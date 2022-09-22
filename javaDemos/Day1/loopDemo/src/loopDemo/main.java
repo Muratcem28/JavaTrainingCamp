2102cb360d96691da9281a27d2bf4df96988d057
Murat Cem <muratcemozdemir28@gmail.com>
2022-09-22 17:02:07 +0300

    First Change
    I divided into days

---
+++ javaDemos/Day1/loopDemo/src/loopDemo/main.java
@@ -0,0 +1,35 @@
+package loopDemo;
+
+public class main {
+
+	public static void main(String[] args) {
+		for (int i = 2; i < 10; i += 2) {
+			System.out.println(i);
+		}
+		System.out.println("Loop Complete");
+
+		// -----------------------
+
+		int i = 1;
+		while (i < 10) {
+			System.out.println(i);
+			i++;
+			// tek sayıları yazdırmak için i+=2, çift sayiları yazdırmak için i = 0 ve i+=2
+		}
+		System.out.println("While Loop Complete");
+
+		// infinite loop = sonsuz döngü --> i sayacini 1 arttırmasaydık döngü koşulunu
+		// asla sağlayamacağı için sonsuza kadar devam edecekti
+		// ----------------------
+
+		int j = 1;
+		do {
+			System.out.println(j);
+			j += 2;
+		} while (j < 10);
+		System.out.println("Do While Loop Complete");
+		///// while dan farkı şart sağlanmasa bile bir kez çalışacaktır. Kullanım alanı
+		///// : veritabanına log atarken kullanılabilir
+	}
+
+}
